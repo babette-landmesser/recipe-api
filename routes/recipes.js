ce529ff6a67e6ac4b47d2f64a0aa82642beb2418
Babette Landmesser <babette.landmesser@cocomore.com>
2018-03-03 00:06:33 +0100

    access /recipes endpoint only with valid jwt token

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
+require('../config/config')
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+const jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+jwtOptions.secretOrKey = CONFIG.jwt_encryption;
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+   console.log('payload received', jwt_payload);
+
+   connection.query('SELECT * from users WHERE id="' + jwt_payload.id + '"', function (error, user, fields) {
+      if(error || user.length === 0){
+         next(null, false);
+      } else {
+         if (user.length === 1) {
+            next(null, user[0]);
+         }
+
+      }
+   });
+});
+
+passport.use(strategy);
 
 /* GET recipes listing. */
-router.get('/', function(req, res, next) {
+router.get('/', passport.authenticate('jwt', { session: false }), function(req, res, next) {
    connection.query('SELECT * from recipes', function (error, results, fields) {
       if(error){
          res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
