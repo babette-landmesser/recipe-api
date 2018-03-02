828bd77ab53e6503d7e85881a13e84b9cd6f973a
Babette Landmesser <babette.landmesser@cocomore.com>
2018-03-02 23:48:47 +0100

    Initial commit

---
+++ data/users.js
@@ -0,0 +1,14 @@
+var users = [
+   {
+      id: 1,
+      name: 'blandmesser',
+      password: 'admin'
+   },
+   {
+      id: 2,
+      name: 'test',
+      password: 'test'
+   }
+];
+
+module.exports = users;
---
+++ routes/login.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+var jwt = require('jsonwebtoken');
+require('../config/config');
+
+var passportJWT = require("passport-jwt");
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+const jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
+jwtOptions.secretOrKey = CONFIG.jwt_encryption;
+
+router.post('/', function (req, res) {
+   if (req.body.name && req.body.password) {
+      const name = req.body.name;
+      const password = req.body.password;
+
+      connection.query('SELECT * from users WHERE username="' + name + '"', function (error, results, fields) {
+         if(error || results.length === 0){
+            res.status(401).json({message: "no such user found"});
+         } else {
+            if (results.length === 1 && results[0].password === password) {
+               const user = results[0];
+               const payload = {id: user.id};
+               const token = jwt.sign(payload, jwtOptions.secretOrKey);
+               res.json({message: "ok", token: token});
+            } else {
+               res.status(401).json({message: 'passwords did not match'});
+            }
+         }
+      });
+   } else {
+      res.status(401).json({message: 'enter data to login'});
+   }
+});
+
+module.exports = router;
